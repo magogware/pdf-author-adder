152614798c96d9f9f3984cf502d38d6b75319b13
magogware <55420093+magogware@users.noreply.github.com>
2019-10-03 21:07:55 +1000

    Added script

---
+++ pdf_renamer.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+date=$(date +%Y%m%d%H%M%S)
+for f in *pdf
+do
+	# Give a status update
+	echo -n "Processing file $f... "
+
+	# Convert file name to a title
+	title=$(python3 file_to_title.py $f)
+
+	# Write metadata to pdfmark file
+	echo "[ /Title ($title)" > docinfo.pdfmark
+	echo "  /Author ($1)" >> docinfo.pdfmark
+	echo "  /Subject (PDF)" >> docinfo.pdfmark
+	echo "  /Creator (PDF Renamer)" >> docinfo.pdfmark
+	echo "  /ModDate (D:$date+10'00')" >> docinfo.pdfmark
+	echo "  /Producer (Ghostscript)" >> docinfo.pdfmark
+	echo "  /Keywords (Ghostscript, PDF)" >> docinfo.pdfmark
+	echo "  /CreationDate (D:$date+10'00')" >> docinfo.pdfmark
+	echo "  /DOCINFO" >> docinfo.pdfmark
+	echo "pdfmark" >> docinfo.pdfmark
+
+	# Overwrite file with metadata
+	gs -q -o new_$f -sDEVICE=pdfwrite $f docinfo.pdfmark 
+
+	# Remove pdfmark file
+	rm docinfo.pdfmark
+
+	# Status update
+	echo "Done."
+done
+
+
+
