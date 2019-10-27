47bcb1863bc6c304c0529108442947ccbf35c9a8
Alex Barilaro <a_barilaro@pm.me>
2019-10-27 20:17:26 +1100

    Made arguments more intuitive, fixed not checking correct usage

--- pdf_renamer.sh
+++ pdf_renamer.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+# Get current date
 date=$(date +%Y%m%d%H%M%S)
-for f in *pdf
-do
+
+# Check script is being used properly
+if [ "$#" < 3 ]; then
+	echo "Usage: pdf_renamer.sh authorname pdf(s)"
+	exit 1
+fi
+
+# Get author and delete it from
+# the array of arguments
+author=$1
+shift
+
+# Loop through files given
+# as arguments
+while (( "$#" )); do
+
 	# Give a status update
-	echo -n "Processing file $f... "
+	echo -n "Processing file $1... "
 
 	# Convert file name to a title
-	bare=$(basename --suffix=".pdf" $f)
+	bare=$(basename --suffix=".pdf" $1)
 	title=$(echo $bare | tr _ ' ' | sed -e "s/\b\(.\)/\u\1/g")
 
 	# Write metadata to pdfmark file
@@ -23,14 +38,19 @@ do
 	echo "pdfmark" >> docinfo.pdfmark
 
 	# Overwrite file with metadata
-	gs -q -o new_$f -sDEVICE=pdfwrite $f docinfo.pdfmark 
+	gs -q -o new_$1 -sDEVICE=pdfwrite $1 docinfo.pdfmark 
+
+	# Overwrite old file
+	rm $1
+	mv new_$1 $1
 
 	# Remove pdfmark file
 	rm docinfo.pdfmark
 
 	# Status update
 	echo "Done."
+
+	# Remove this file from the array
+	shift
 done
-
-
-
+exit 0
